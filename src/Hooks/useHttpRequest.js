ca7e349f79b7545767d75f1c81bd3008f44b58e0
Savvas <savaravant@gmail.com>
2021-05-22 01:45:52 +0300

    m5

---
+++ src/Hooks/useHttpRequest.js
@@ -0,0 +1,37 @@
+import { useCallback, useState } from 'react';
+
+const useHttpRequest = () => {
+    const [error, setError] = useState(null);
+    const [isLoading, setIsLoading] = useState(false);
+
+    const changeOptionHandler = useCallback(async (abortController, requestConfiguration, workWithData) => {
+        setIsLoading(true);
+        setError(null);
+        try {
+            const response = await fetch(requestConfiguration.url, {
+                signal: abortController.signal
+            });
+            if (!response.ok) {
+                throw new Error("An error occured");
+            }
+            const data = await response.json();
+            console.log(data)
+            workWithData(data);
+        } catch (error) {
+            if (abortController.signal.aborted) {
+                setError(error.message);
+            } else {
+                throw error;
+            }
+        }
+        setIsLoading(false);
+    }, [])
+
+    return {
+        error: error,
+        isLoading: isLoading,
+        changeOptionHandler: changeOptionHandler
+    }
+}
+
+export default useHttpRequest;
--- src/components/HeaderComponents/Location.js
+++ src/components/HeaderComponents/Location.js
@@ -1,23 +1,15 @@
-import React, { useCallback, useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import styles from './Location.module.css';
+import useHttpRequest from '../../Hooks/useHttpRequest';
 
 const Location = () => {
-    const [error, setError] = useState(null);
-    const [isLoading, setIsLoading] = useState(false);
     const [cities, setCities] = useState([]);
+    const { error, isLoading, changeOptionHandler: changeLocationHandler } = useHttpRequest();
 
-    const changeLocationHandler = useCallback(async abortController => {
-        const newObject = {};
-        setIsLoading(true);
-        setError(null);
-        try {
-            const response = await fetch('https://mybooking-28176-default-rtdb.firebaseio.com/1.json', {
-                signal: abortController.signal
-            });
-            if (!response.ok) {
-                throw new Error("An error occured");
-            }
-            const data = await response.json();
+    useEffect(() => {
+        const abortController = new AbortController();
+        const workWithCitiesData = async data => {
+            const newObject = {}
             const result = await data.entries.map(hotelData => {
                 return {
                     location: hotelData.city
@@ -27,25 +19,16 @@ const Location = () => {
                 !newObject[object.location] && (newObject[object.location] = true)
             );
             setCities(filteredArray);
-        } catch (error) {
-            if (abortController.signal.aborted) {
-                setError(error.message);
-            } else {
-                throw error;
-            }
         }
-        setIsLoading(false);
-    }, [])
-
-    useEffect(() => {
-        const abortController = new AbortController();
-        changeLocationHandler(abortController);
+        changeLocationHandler(abortController, {
+            url: 'https://mybooking-28176-default-rtdb.firebaseio.com/1.json'
+        }, workWithCitiesData);
         return () => abortController.abort();
     }, [changeLocationHandler])
 
     return (
         <div className={styles.Location}>
-            <select onChange={changeLocationHandler} >
+            <select onChange={changeLocationHandler.bind(null, new AbortController())} >
                 {!isLoading && error && <p>{error}</p>}
                 {
                     cities.map((cityName, index) =>
